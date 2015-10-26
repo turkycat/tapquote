cba737cda277cd91e8d3b9e8d5278e47773145c7
Jesse Frush <jesse.frush@gmail.com>
2015-10-25 17:45:27 -0700

    Set the recycler view up to use a Grid layout manager

--- taps/app/src/main/java/turkycat/taps/fragments/RecyclerViewFragment.java
+++ taps/app/src/main/java/turkycat/taps/fragments/RecyclerViewFragment.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,9 +20,13 @@ import turkycat.taps.R;
  */
 public class RecyclerViewFragment extends Fragment
 {
-    public static String TAG = "RecyclerViewFragment";
+    public final static String TAG = "RecyclerViewFragment";
 
     private OnFragmentInteractionListener mListener;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private final int numberOfCols = 2;
 
     public RecyclerViewFragment()
     {
@@ -33,7 +39,13 @@ public class RecyclerViewFragment extends Fragment
                               Bundle savedInstanceState )
     {
         // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_recycler_view, container, false );
+        View view = inflater.inflate( R.layout.fragment_recycler_view, container, false );
+        recyclerView = (RecyclerView) view.findViewById( R.id.recyclerView );
+
+        //set the layout manager to a grid manager
+        layoutManager = new GridLayoutManager( getContext(), numberOfCols );
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -64,6 +76,8 @@ public class RecyclerViewFragment extends Fragment
     {
         super.onDetach();
         mListener = null;
+        recyclerView = null;
+        layoutManager = null;
     }
 
     /**
