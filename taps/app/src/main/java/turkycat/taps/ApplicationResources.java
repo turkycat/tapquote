dc9af56327e4ed08e35c17f952ebe6d19ba4da58
Jesse Frush <jesse.frush@gmail.com>
2015-10-25 18:09:37 -0700

    Added ApplicationResources singleton and Tap class

---
+++ taps/app/src/main/java/turkycat/taps/ApplicationResources.java
@@ -0,0 +1,38 @@
+package turkycat.taps;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import android.support.v4.content.ContextCompat;
+import java.util.LinkedList;
+import java.util.List;
+
+import turkycat.taps.taps.Tap;
+
+/**
+ * Created by turkycat on 10/25/2015.
+ */
+public class ApplicationResources
+{
+    //singleton class
+    private static ApplicationResources instance = new ApplicationResources();
+    private List<Tap> tapList;
+
+    public static ApplicationResources getInstance()
+    {
+        return instance;
+    }
+
+    public void initialize( Context context )
+    {
+        AssetManager assets = context.getAssets();
+
+        tapList.add( new Tap( "#TD4W", ContextCompat.getDrawable( context, R.drawable.td4w ) ) );
+    }
+
+    //private singleton constructor
+    private ApplicationResources()
+    {
+        tapList = new LinkedList<>();
+    }
+}
---
+++ taps/app/src/main/java/turkycat/taps/taps/Tap.java
@@ -0,0 +1,18 @@
+package turkycat.taps.taps;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by turkycat on 10/25/2015.
+ */
+public class Tap
+{
+    private String name;
+    private Drawable drawable;
+
+    public Tap( String name, Drawable drawable )
+    {
+        this.name = name;
+        this.drawable = drawable;
+    }
+}
